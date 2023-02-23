b3b894cebdc1d980a3607511d12b424a20f0e653
ALVIN KAMAL <alvinkn.dev@gmail.com>
2023-02-23 12:41:52 +0700

    create register page

---
+++ lib/view/register_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
